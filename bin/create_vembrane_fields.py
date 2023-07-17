1d3a31841edcefb5472977c30160a06a1f9d4df6
Kai Horny <63246519+sci-kai@users.noreply.github.com>
2023-07-17 17:16:36 +0200

    Rewrote vembrane python script. More clear argument parsing, added input_check, better separation of concerns.

--- bin/create_vembrane_fields.py
+++ bin/create_vembrane_fields.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
 import re
-import sys
 import argparse
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='The general purpose of this script is to easily convert annotation fields into vembrane input parameters. \
-        The input are a comma-separated field names for your annotation or from the VCF file. \
-        It output has the format: column_name["input_field"][sampleindex] for each field of input_fields. \
+        The input are field names from the INFO/FORMAT column or annotation string as CSQ in the VCF file. \
+        The output has the format: column_name["input_field"][sampleindex] for each field of input_fields. \
         You can also directly perform calculation operations which will be transcribed to vembrane-compatible fields. \
-        , e.g. , AD[0]/DP will be converted to column_name["AD"][sampleindex][0]/column_name["DP"][sampleindex]. \
-        Vembrane interprets this operation and divides the AD value by the DP value.'
+        , e.g. , AD[1]/DP will be converted to column_name["AD"][sampleindex][0]/column_name["DP"][sampleindex]. \
+        Vembrane interprets this operation and divides the AD value by the DP value to get the allele fraction.'
     )
     parser.add_argument(
         "input_fields",
-        help="string with comma-separated input fields. The fields need to be letters separated by non-letters (e.g. mathematical operations).",
+        help="space-separated input fields. The fields only allow letters, numbers, underscores, square brackets and mathematical operands.",
         type=str,
-        nargs=1,
+        nargs="+",
     )
     parser.add_argument(
         "--column_name",
         help="string that specifies the location of the input_field for vembrane, e.g., FORMAT, INFO or CSQ.",
         type=str,
         nargs=1,
-        default=None
+        default=None,
     )
     parser.add_argument(
         "--fields_with_sampleindex",
@@ -35,84 +35,92 @@ def parse_arguments():
     )
     parser.add_argument(
         "--sampleindex",
-        help="integer sample index for all fields_with_sampleindex."
+        help="integer sample index for all fields_with_sampleindex.",
         type=int,
         nargs=1,
         default=0,
     )
     parser.add_argument(
-        "--header_prefix",
-        help="if enabled, instead of formatting as described only outputs defined prefix to the string.",
-        type=str,
-        nargs=1,
-        default=None,
+        "--header",
+        help="if enabled, instead of formatting as described only outputs fields that can be used as header for vembrane.",
+        action="store_true",
     )
 
     return parser.parse_args()
 
-def format_string(
-    input_fields,
+def input_check(input_strings):
+    for input_string in input_strings:
+        if re.match(r"^[A-Za-z0-9+\-*/\[\]_]+$", input_string) is None:
+            raise ValueError(f"Error: The input_fields {input_string} should only contain letters, numbers, square brackets, mathematical operands or underscores.")
+
+def format_field(
+    input_field,
     column_name,
-    fields_with_sampleindex=None,
-    sampleindex=None,
-    header_prefix=None,
+    fields_with_sampleindex,
+    sampleindex,
+    header,
 ):
-    if fields_with_sampleindex is None and sampleindex is not None:
-        fields_with_sampleindex = "all"
+    # formatting fields for vembrane input
+    def create_field(field_string):
+        result = f'{column_name[0]}["{field_string}"]'
+        # if sampleindex is defined, it will be appended
+        if field_string in fields_with_sampleindex or "all" in fields_with_sampleindex:
+            result += f"[{sampleindex[0]}]"
+        return result
 
-    def field_format(match):
-        column_name = match.group(1)
-        if header_prefix is not None:
-            return _format_header(column_name, header_prefix)
-        elif fields_with_sampleindex == "all":
-            if sampleindex is not None:
-                return _format_field(column_name, sampleindex)
-            else:
-                raise ValueError(
-                    "Sample index needs to be specified when using fields_with_sampleindex == all."
-                )
-        elif (
-            fields_with_sampleindex
-            and column_name in fields_with_sampleindex is not None
-        ):
-            if sampleindex is not None:
-                return _format_field(column_name, sampleindex)
-            else:
-                raise ValueError(
-                    "Sample index needs to be specified when using fields_with_sampleindex."
-                )
+    # Formatting fields for header lines: only column_name underscore field
+    def create_header(field_string):
+        result = f"{column_name[0]}_{field_string}"
+        # if sampleindex is defined, it will be appended
+        if field_string in fields_with_sampleindex or "all" in fields_with_sampleindex:
+            result += f"[{sampleindex[0]}]"
+        return result
+
+    def process_field(mathsplit_field, input_field_formatted):
+        # only use letters and underscores (no brackets and numbers)
+        only_field = re.sub(r"[^A-Za-z_]", "", mathsplit_field)
+        if header is True:
+            # create header format
+            only_field_formatted = create_header(only_field)
         else:
-            return _format_field(column_name)
+            # create field format
+            only_field_formatted = create_field(only_field)
+        # replace from original field; this preserves brackets and numbers from the input.
+        input_field_formatted = re.sub(
+            only_field, only_field_formatted, input_field_formatted
+        )
+        return input_field_formatted
 
-    def _format_field(field, sampleindex=None):
-        result = f'{column_name}["{field}"]'
-        if sampleindex is not None:
-            result += f"[{sampleindex}]"
-        return result
+    # first split input by mathematical operands
+    mathsplit_fields = re.split(r"[+\-*/]", input_field)
+    # define format_field that will be subsequently manipulated
+    input_field_formatted = input_field
+    for mathsplit_field in mathsplit_fields:
+        # allow number-only fields and do not format them
+        if re.match(r"[0-9]", mathsplit_field) is not None:
+            pass
+        else:
+            input_field_formatted = process_field(mathsplit_field, input_field_formatted)
 
-    def _format_header(field, header_prefix):
-        result = f"{header_prefix}_{field}"
-        return result
-
-    formatted_string = re.sub(r"([A-Za-z_]+)", field_format, input_fields)
-    return formatted_string
-
+    return input_field_formatted
 
 if __name__ == "__main__":
-
     args = parse_arguments()
 
-    # Format the string
-    try:
-        output_string = format_string(
-            args.input_fields,
+    input_check(args.input_fields)
+
+    # Format the input fields
+    output_string = ""
+    for input_field in args.input_fields:
+        input_field_formatted = format_field(
+            input_field,
             args.column_name,
             args.fields_with_sampleindex,
             args.sampleindex,
-            args.header_prefix,
+            args.header,
         )
-    except ValueError as e:
-        print(f"Error: {str(e)}")
-        sys.exit(1)
+        output_string = output_string + input_field_formatted
+        if(input_field != args.input_fields[-1]):
+            output_string = output_string + ","
 
     print(output_string)
