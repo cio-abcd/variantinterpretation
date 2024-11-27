Fixed TMB-calculation and changed BED processing/fixed VCFPROC subworkflow

bin/calculate_TMB.py
@@ -27,6 +27,12 @@ def parse_args(argv=None):
         help="Input tsv file of the vembrane TSV converter module",
     )
     parser.add_argument(
+        "--sampleindex",
+        metavar="sampleindex",
+        type=str,
+        help="sampleindex based on meta.id of the input file"
+    )
+    parser.add_argument(
         "--bedfile",
         metavar="bedfile",
         type=Path,
@@ -99,8 +105,18 @@ def parse_args(argv=None):
 
 logger = logging.getLogger()
 
+def multisample_check(file_in):
+    ### Read in sample
+    TMB_multicontrol = pd.read_csv(file_in, sep="\t")
+    ### Generate boolean based on AF column if single- or multisample
+    multi_control = len(TMB_multicontrol.filter(regex='allele_fraction*').columns) > 1 ### check based on AF
+    ### Isolate sample suffix(es)
+    matching_columns = [col for col in TMB_multicontrol.columns if col.startswith('allele_fraction')]
+    suffix_list = list(map( lambda x: x.replace('allele_fraction', ''), matching_columns))
+    ### Return all relevant variables
+    return (multi_control, suffix_list)
 
-def preprocess_vembraneout(file_in, filter_muttype, population_db):
+def preprocess_vembraneout(file_in, allele_fraction, read_depth, filter_muttype, population_db):
     TMB_inputfile = pd.read_csv(file_in, sep="\t")
     TMB_inputfile["Mut_ID"] = TMB_inputfile[["CHROM", "POS", "REF", "ALT"]].apply(
         lambda row: ":".join(row.values.astype(str)), axis=1
@@ -117,8 +133,8 @@ def preprocess_vembraneout(file_in, filter_muttype, population_db):
             "REF",
             "ALT",
             "FILTER",
-            "allele_fraction",
-            "read_depth",
+            allele_fraction,
+            read_depth,
             "CSQ_VARIANT_CLASS",
             "CSQ_Consequence",
             population_db,
@@ -197,16 +213,16 @@ def check_bed_size(bedfile, breaking_thresh):
         return False, False
 
 
-def coverage_filter(input, threshold):
-    TMB_covfilt = input[(input["read_depth"] >= threshold)].reset_index(drop=True)
+def coverage_filter(input, read_depth, threshold):
+    TMB_covfilt = input[(input[read_depth] >= threshold)].reset_index(drop=True)
     filtering_rates = len(TMB_covfilt["Mut_ID"])
     return (TMB_covfilt, filtering_rates)
 
 
-def allelefrequency_filter(input, lower_threshold, higher_threshold):
+def allelefrequency_filter(input, allele_fraction, lower_threshold, higher_threshold):
     TMB_AFboundariesfilt = input[
-        (input["allele_fraction"] >= lower_threshold)
-        & (input["allele_fraction"] <= higher_threshold)
+        (input[allele_fraction] >= lower_threshold)
+        & (input[allele_fraction] <= higher_threshold)
     ].reset_index(drop=True)
     filtering_rates = len(TMB_AFboundariesfilt["Mut_ID"])
     return (TMB_AFboundariesfilt, filtering_rates)
@@ -222,8 +238,97 @@ def calculate_TMB(input, panel_size):
     TMB = round((len(input.index) / panel_size) * 1000000, 2)
     return TMB
 
+def process_single_sample(args, suffix):
+    ### Wrapper for single-sample TSV
+    allele_fraction = f'allele_fraction{suffix}'
+    read_depth = f'read_depth{suffix}'
+    TMB_df, filtering_rates_total = preprocess_vembraneout(
+        args.file_in, allele_fraction, read_depth, args.filter_muttype, args.population_db
+    )
+    process_data(args, TMB_df, filtering_rates_total, allele_fraction, read_depth, args.file_out, args.plot_out)
 
-def plot_TMB(input, name_convention, lower_af, higher_af):
+
+def process_multi_sample(args, suffix_list):
+    ### Wrapper for multi-sample TSV
+    ### Outputs each sample in the VCF/TSV as seperate file
+    for suffix in suffix_list:
+        allele_fraction = f'allele_fraction{suffix}'
+        read_depth = f'read_depth{suffix}'
+        output_filename = f"{str(args.file_out).strip('.txt')}_{suffix}.txt"
+        output_plotname = f"{str(args.plot_out).strip('.png')}_{suffix}.png"
+        TMB_df, filtering_rates_total = preprocess_vembraneout(
+            args.file_in, allele_fraction, read_depth, args.filter_muttype, args.population_db
+        )
+        process_data(args, TMB_df, filtering_rates_total, allele_fraction, read_depth, output_filename, output_plotname)
+
+
+def process_data(args, TMB_df, filtering_rates_total, allele_fraction, read_depth, output_file, output_plot):
+    ### Basically what main did before
+    ### Processes data based on thresholds and filter conditions to generate the output txt files
+    is_eligible, panel_size = check_bed_size(args.bedfile, args.panelsize_threshold)
+
+    if not is_eligible:
+        logger.info(
+            "The calculation was not performed as the panel_size is below the allowed threshold."
+        )
+        return
+
+    TMB_covfilt, filtering_rates_cov = coverage_filter(TMB_df, read_depth, args.min_cov)
+    TMB_affilt, filtering_rates_af = allelefrequency_filter(
+        TMB_covfilt, allele_fraction, args.min_AF, args.max_AF
+    )
+    TMB_popfilt, filtering_rates_popfreq = popfrequency_filter(
+        TMB_affilt, args.population_db, args.popfreq_max
+    )
+    TMB_value = calculate_TMB(
+        TMB_popfilt, panel_size
+    )
+    plot_TMB(
+        TMB_covfilt, output_plot, allele_fraction, args.min_AF, args.max_AF
+    )
+
+    filtering_rates_total += [
+        filtering_rates_cov,
+        filtering_rates_af,
+        filtering_rates_popfreq,
+    ]
+
+    write_output(
+        output_file,
+        filtering_rates_total,
+        args.filter_muttype,
+        args.min_cov,
+        args.min_AF,
+        args.max_AF,
+        args.population_db,
+        args.popfreq_max,
+        TMB_value,
+        panel_size,
+    )
+
+def write_output(
+    output_file, filtering_rates, filter_muttype, min_cov, min_AF, max_AF, population_db, popfreq_max, TMB_value, panel_size
+):
+    with open(output_file, "w") as file:
+        file.write(f"Initial amount of unique mutations: {filtering_rates[0]} mutations\n")
+        if filter_muttype in ["snv", "snvs", "mnv", "mnvs"]:
+            file.write(
+                f"Retained mutations after filtering for mutation type {filter_muttype}: {filtering_rates[1]} mutations\n"
+            )
+        file.write(
+            f"Retained mutations after applying coverage filter for a threshold below {min_cov}: {filtering_rates[2]} mutations\n"
+        )
+        file.write(
+            f"Retained mutations after filtering for AF between {min_AF} and {max_AF}: {filtering_rates[3]} mutations\n"
+        )
+        file.write(
+            f"Retained mutations after filtering for the population AF from {population_db} below {popfreq_max}: {filtering_rates[4]} mutations\n"
+        )
+        file.write(
+            f"The final TMB based on these filtering conditions and a panel size of {panel_size}: {TMB_value} mutations/Mbp"
+        )
+
+def plot_TMB(input, output_plotname, allele_fraction, lower_af, higher_af):
     ### Preprocess for plotting
     counts_consequence = input.groupby(
         input.columns.tolist(), as_index=False, dropna=False
@@ -258,16 +363,11 @@ def plot_TMB(input, name_convention, lower_af, higher_af):
         stat="count",
         multiple="stack",
         binwidth=0.01,
-        x="allele_fraction",
+        x=allele_fraction,
         kde=False,
         palette="colorblind",
         hue="CSQ_Consequence",
         element="bars",
-    )
-
-    ### Control the legend
-    sns.move_legend(
-        p, loc="center right", bbox_to_anchor=(1.35, 0.5), ncol=1, fontsize=6
     )
 
     ### Show filtering parameters
@@ -280,69 +380,33 @@ def plot_TMB(input, name_convention, lower_af, higher_af):
     ax.set_ylabel("# of mutations")
     plt.xticks(np.arange(0, 1.1, 0.1))
     ax.yaxis.get_major_locator().set_params(integer=True)  ## force integers on y-axis
-    plt.savefig(name_convention, bbox_inches="tight")
+    plt.savefig(output_plotname, bbox_inches="tight")
 
+##########
+#### MAIN
+##########
 
 def main(argv=None):
-    """Coordinate argument parsing and program execution."""
+    ### Parse arguments
     args = parse_args(argv)
+
     if not args.file_in.is_file():
         logger.error(f"The given input file {args.file_in} was not found!")
+        return
 
-    # Need to adjust parameters of zero is passed to avoid None input
-    if args.min_cov is None:
-        args.min_cov = 0.00
-    if args.min_AF is None:
-        args.min_AF = 0.00
-    if args.panelsize_threshold is None:
-        args.panelsize_threshold = 0
+    # Set default parameters if they are None
+    args.min_cov = args.min_cov or 0.00
+    args.min_AF = args.min_AF or 0.00
+    args.panelsize_threshold = args.panelsize_threshold or 0
 
-    TMB_df, filtering_rates_total = preprocess_vembraneout(
-        args.file_in, args.filter_muttype, args.population_db
-    )
-    eligibility, panel_size = check_bed_size(args.bedfile, args.panelsize_threshold)
-    if eligibility == True:
-        TMB_covfilt, filtering_rates_cov = coverage_filter(TMB_df, args.min_cov)
-        TMB_affilt, filtering_rates_af = allelefrequency_filter(
-            TMB_covfilt, args.min_AF, args.max_AF
-        )
-        TMB_popfilt, filtering_rates_popfreq = popfrequency_filter(
-            TMB_affilt, args.population_db, args.popfreq_max
-        )
-        TMB_value = calculate_TMB(TMB_popfilt, panel_size)
-        filtering_rates_total = filtering_rates_total + [
-            filtering_rates_cov,
-            filtering_rates_af,
-            filtering_rates_popfreq,
-        ]
-        plot_TMB(
-            TMB_covfilt, args.plot_out, args.min_AF, args.max_AF
-        )  ### Use coverage filtered data as non-filtered data compresses plot to uselessness...
-        with open(args.file_out, "w") as file:
-            file.write(
-                f"Inital amount of unique mutations: {filtering_rates_total[0]} mutations\n"
-            )
-            if args.filter_muttype in ["snv", "snvs", "mnv", "mnvs"]:
-                file.write(
-                    f"Retained mutations after filtering for mutation type {args.filter_muttype}: {filtering_rates_total[1]} mutations\n"
-                )
-            file.write(
-                f"Retained mutations after applying coverage filter for a threshold below {args.min_cov}: {filtering_rates_total[2]} mutations\n"
-            )
-            file.write(
-                f"Retained mutations after filtering for AF between {args.min_AF} and {args.max_AF}: {filtering_rates_total[3]} mutations\n"
-            )
-            file.write(
-                f"Retained mutations after filtering for the population AF from {args.population_db} below {args.popfreq_max}: {filtering_rates_total[4]} mutations\n"
-            )
-            file.write(
-                f"The final TMB based on these filtering conditions and a panel size of {panel_size}: {TMB_value} mutations/Mbp"
-            )
+    # Check if the input is a single-sample or multi-sample TSV report
+    is_multi, suffix_list = multisample_check(args.file_in)
+
+    # Split workflow based on single or multi-sample TSV
+    if not is_multi:
+        process_single_sample(args, suffix_list[0])
     else:
-        logger.info(
-            "The calculation was not performed as the panel_size is below the allowed threshold."
-        )
-
+        process_multi_sample(args, suffix_list)
 
 if __name__ == "__main__":
     sys.exit(main())